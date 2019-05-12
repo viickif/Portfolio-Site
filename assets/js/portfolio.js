d16c0725cf70961940b5b0a9d09111d28a2ebf6b
viickif <vicki.wfu@gmail.com>
2019-05-12 14:18:23 -0700

    implemented snap scroll

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,17 +1,29 @@
-$(document).ready(function(){
 
+
+
+// $('#loadingDiv').bind('ajaxStart', function(){
+//     $(this).show();
+// }).bind('ajaxStop', function(){
+//     $(this).hide();
+// })
+
+// $(window).on('load', function() {
+// 	$('#loadingDiv').css("display", "block");
+// })
+$(document).ready(function(){
 	var homeText = $('#home h1');
 
 	$(homeText[0]).slideDown(700, function() {
-		$(homeText[1]).slideDown(500);
-		$(homeText[2]).slideDown(500, function() { window.setTimeout(function(){
+		$(homeText[1]).slideDown(300);
+		$(homeText[2]).slideDown(300, function() { window.setTimeout(function(){
 				$('#home p').slideDown(150);
 				$('.navbar').slideDown(150);
-			},250); 
+			},300); 
 		});
 	});
 
-	$(window).on("scroll", function() {
+	$(window).on('scroll', function(e) {
+		e.preventDefault();
     	if($(window).scrollTop() > $(window).height()){
     		$('.bg-light').addClass("inverse-nav");
     		$('li a').addClass("inverse-nav-link");
@@ -19,14 +31,20 @@ $(document).ready(function(){
     		$('.bg-light').removeClass("inverse-nav");
     		$('li a').removeClass("inverse-nav-link");
     	}
+
+    	// $('html, body').animate({
+     //    	scrollTop: $("#projects").offset().top
+    	// }, 40);
+
+
 	}); 
 
 
-
-
-
-
-
+	$("h1").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#projects").offset().top
+    }, 40);
+});
 
 
 
