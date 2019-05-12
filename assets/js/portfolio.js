7f1740eb749294332511833645ff68bf5b4426f5
viickif <vicki.wfu@gmail.com>
2019-05-11 23:22:30 -0700

    dynamic nav and img

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -2,18 +2,23 @@ $(document).ready(function(){
 
 	var homeText = $('#home h1');
 
-	$(homeText[0]).slideDown(300, function() {
-		$(homeText[1]).slideDown(300, function() {
-			$(homeText[2]).slideDown(300, function() {
-				$('#home p').slideDown(150);
-				$('.navbar').slideDown(150);
-			});
+	$(homeText[0]).slideDown(359, function() {
+		$(homeText[1]).slideDown(350);
+		$(homeText[2]).slideDown(350, function() {
+			$('#home p').slideDown(150);
+			$('.navbar').slideDown(150);
 		});
 	});
 
-	$(window).on('scroll', function() {
-		console.log("hi");
-	});
+	$(window).on("scroll", function() {
+    	if($(window).scrollTop() > $(window).height()){
+    		$('.bg-light').addClass("inverse-nav");
+    		$('li a').addClass("inverse-nav-link");
+    	} else {
+    		$('.bg-light').removeClass("inverse-nav");
+    		$('li a').removeClass("inverse-nav-link");
+    	}
+	}); 
 
 
 
