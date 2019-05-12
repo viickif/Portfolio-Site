0d23fa46f289b3c0448b2c5900fbe40cbb8ec018
viickif <vicki.wfu@gmail.com>
2019-05-11 18:39:11 -0700

    home page complete

---
+++ assets/js/portfolio.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	var homeText = $('#home h1');
+
+	$(homeText[0]).slideDown(300, function() {
+		$(homeText[1]).slideDown(300, function() {
+			$(homeText[2]).slideDown(300, function() {
+				$('#home p').slideDown(150);
+				$('.navbar').slideDown(150);
+			});
+		});
+	});
+
+	$('#home').on('scroll', function() {
+		
+	})
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
