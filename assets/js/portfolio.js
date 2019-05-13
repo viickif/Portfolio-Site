4f62cade98164ca1c66709759a51a96d1c1d66ff
viickif <vicki.wfu@gmail.com>
2019-05-12 20:55:46 -0700

    fixed cross browser scroll animation problem

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,15 +1,4 @@
 
-
-
-// $('#loadingDiv').bind('ajaxStart', function(){
-//     $(this).show();
-// }).bind('ajaxStop', function(){
-//     $(this).hide();
-// })
-
-// $(window).on('load', function() {
-// 	$('#loadingDiv').css("display", "block");
-// })
 $(document).ready(function(){
 
 	var homeText = $('#home h1');
@@ -37,9 +26,11 @@ $(document).ready(function(){
     	var project1Top = $('#project1').offset().top;
     	var project2Top = $('#project2').offset().top;
     	var contactTop = $('#contact').offset().top;
+    	var height = $(window).height();
+    	var range = $(window).width() / 5;
 
 
-    	if(homeTop == 0 ){
+    	if(Math.abs(homeTop) < range){
 			$(homeText[0]).slideDown(700, function() {
 				$(homeText[1]).slideDown(300);
 				$(homeText[2]).slideDown(300, function() { window.setTimeout(function(){
@@ -48,38 +39,44 @@ $(document).ready(function(){
 					},300); 
 				});
 			});
-    	} else if (aboutTop == 0) {
-    		homeText.slideUp();
+    	} else if (Math.abs(homeTop) > height) {
+    		homeText.slideUp(1);
     		$('#home p').slideUp(1);
     	}
 
-    	if(aboutTop == 0 ){
-    		$('#about .text p').slideDown(200);
-    	} else if (projectsTop == 0 || homeTop == 0) {
+    	if(Math.abs(aboutTop) < range){
+    		$('#about .text p').slideDown(300);
+    	} else if (Math.abs(aboutTop) > height) {
     		$('#about .text p').slideUp(1);
     	}
 
-    	if(projectsTop == 0 ){
+    	if(Math.abs(projectsTop) < range){
     		$('#projects p').slideDown(200, function() {
 	    			window.setTimeout(function(){
 					$('#projects h1').slideDown(200);
 				},300); 
     		});
-    	} else if (projectsTop == 0 || project2Top == 0) {
+    	} else if (Math.abs(projectsTop) > height) {
     		$('#projects p').slideUp(1);
     		$('#projects h1').slideUp(1);
     	}
 
-    	if(project1Top == 0 ){
-    		$('#project1 .text p').slideDown(200);
-    	} else if (projectsTop == 0 || project2Top == 0) {
+    	if(Math.abs(project1Top) < range ){
+    		$('#project1 .text p').slideDown(300);
+    	} else if (Math.abs(project1Top) > height) {
     		$('#project1 .text p').slideUp(1);
     	}
 
-    	if(project2Top == 0 ){
-    		$('#project2 .text p').slideDown(200);
-    	} else if (contactTop == 0 || project1Top == 0) {
+    	if(Math.abs(project2Top) < range){
+    		$('#project2 .text p').slideDown(300);
+    	} else if (Math.abs(project2Top) > height) {
     		$('#project2 .text p').slideUp(1);
+    	}
+
+    	if(Math.abs(contactTop) < range ){
+    		$('#contact .text p').slideDown(300);
+    	} else if (Math.abs(contactTop) > height) {
+    		$('#contact .text p').slideUp(1);
     	}
 
 	});
