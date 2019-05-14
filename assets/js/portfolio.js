4e174ecb83207fa21ccd7473e08995f76307854b
viickif <vicki.wfu@gmail.com>
2019-05-13 22:34:37 -0700

    made theme more consistent

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -12,17 +12,7 @@ $(document).ready(function(){
 		});
 	});
 
-	$('h1').on('click', function() {
-		alert("hi");
-	})
-
 	$('.scroll').on('scroll', function(e) {
-		var scrollPos = $('.scroll').scrollTop();
-    	if(scrollPos > $('#about').offset().top + 500){
-    		$('li a').addClass("inverse-nav-link");
-    	} else {
-    		$('li a').removeClass("inverse-nav-link");
-    	}
 
     	var homeTop = $('#home').offset().top;
     	var aboutTop = $('#about').offset().top;
@@ -37,49 +27,28 @@ $(document).ready(function(){
     	if(Math.abs(homeTop) < range){
 			$(homeText[0]).slideDown(700, function() {
 				$(homeText[1]).slideDown(300);
-				$(homeText[2]).slideDown(300, function() { window.setTimeout(function(){
-					$('#home p').slideDown(150);
-					$('.navbar').slideDown(150);
-					},300); 
-				});
+				$(homeText[2]).slideDown(300);
 			});
     	} else if (Math.abs(homeTop) > height) {
     		homeText.slideUp(100);
-    		$('#home p').slideUp(100);
     	}
 
-    	if(Math.abs(aboutTop) < range){
-    		$('#about .text p').slideDown(300);
-    	} else if (Math.abs(aboutTop) > height - 20) {
-    		$('#about .text p').slideUp(100);
+    	var toSlide = $('.section');
+
+    	for(var i = 1; i < toSlide.length; i++) {
+    		if(Math.abs(toSlide.eq(i).offset().top) < range) {
+    			toSlide.eq(i).find('.slide').slideDown(500);
+    		} else if (Math.abs(toSlide.eq(i).offset().top) > height - 20) {
+    			toSlide.eq(i).find('.slide').slideUp(500);
+    		}
     	}
 
-    	if(Math.abs(projectsTop) < range){
-    		$('#projects-header').slideDown(200);
-    	} else if (Math.abs(projectsTop) > height - 20) {
-    		$('#projects-header').slideUp(100);
+    	if(aboutTop >= 10 || (projectsTop <= 0 && project1Top >= 10) || contactTop <= 0) {
+    		$('li a').removeClass("inverse-nav-link");
+    	} else {
+    		$('li a').addClass("inverse-nav-link");
     	}
-
-    	if(Math.abs(project1Top) < range ){
-    		$('#project1 .text p').slideDown(300);
-    	} else if (Math.abs(project1Top) > height - 20) {
-    		$('#project1 .text p').slideUp(100);
-    	}
-
-    	if(Math.abs(project2Top) < range){
-    		$('#project2 .text p').slideDown(300);
-    	} else if (Math.abs(project2Top) > height - 20) {
-    		$('#project2 .text p').slideUp(100);
-    	}
-
-    	if(Math.abs(contactTop) < range ){
-    		$('#contact .text p').slideDown(300);
-    	} else if (Math.abs(contactTop) > height - 20) {
-    		$('#contact .text p').slideUp(100);
-    	}
-
 	});
-
 
 });
 
