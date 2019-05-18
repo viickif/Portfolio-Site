689ddd4585deca784707d94c0afdae848d651dcf
viickif <vicki.wfu@gmail.com>
2019-05-18 15:37:44 -0700

    fixed ui bugs

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
 	var homeText = $('#home h1');
 
 	$(homeText[0]).slideDown(700, function() {
-		$(homeText[1]).slideDown(300);
-		$(homeText[2]).slideDown(300, function() { window.setTimeout(function(){
+		$(homeText[1]).slideDown(500);
+		$(homeText[2]).slideDown(500, function() { window.setTimeout(function(){
 				$('#home p').slideDown(150);
 				$('.navbar').slideDown(150);
 			},300); 
@@ -16,6 +16,9 @@ $(document).ready(function(){
 
     	var homeTop = $('#home').offset().top;
     	var aboutTop = $('#about').offset().top;
+    	var projectsTop = $('#projects').offset().top;
+    	var project1Top = $('#project1').offset().top;
+    	var project2Top = $('#project2').offset().top;
     	var contactTop = $('#contact').offset().top;
     	var height = $(window).height();
     	var range = $(window).width() / 5;
@@ -23,11 +26,15 @@ $(document).ready(function(){
 
     	if(Math.abs(homeTop) < range){
 			$(homeText[0]).slideDown(700, function() {
-				$(homeText[1]).slideDown(300);
-				$(homeText[2]).slideDown(300);
+				$(homeText[1]).slideDown(500);
+				$(homeText[2]).slideDown(500, function() { window.setTimeout(function(){
+						$('#home p').slideDown(150);
+					},300); 
+				});
 			});
     	} else if (Math.abs(homeTop) > height) {
     		homeText.slideUp(100);
+    		$('#home p').slideUp(150);
     	}
 
     	var toSlide = $('.section');
@@ -40,10 +47,13 @@ $(document).ready(function(){
     		}
     	}
 
-    	if(aboutTop >= 10 || (projectsTop <= 0 && project1Top >= 10) || contactTop <= 0) {
+    	if(aboutTop >= 100 || (projectsTop <= 50 && project1Top >= 100) || contactTop <= 50) {
     		$('li a').removeClass("inverse-nav-link");
+    		if(aboutTop < 100 )
+    			$('nav a img').attr("src", "assets/img/logo-white.png");
     	} else {
     		$('li a').addClass("inverse-nav-link");
+    		$('nav a img').attr("src", "assets/img/logo.png");
     	}
 	});
 
